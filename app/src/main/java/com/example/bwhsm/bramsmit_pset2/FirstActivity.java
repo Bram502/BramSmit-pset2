0f2467be8c1db3bdb26766df8d9637fb44ecb240
Bram <bwhsmit2@hotmail.com>
2017-09-08 13:34:12 +0200

    Layout of Main and First activity added

---
+++ app/src/main/java/com/example/bwhsm/bramsmit_pset2/FirstActivity.java
@@ -0,0 +1,24 @@
+package com.example.bwhsm.bramsmit_pset2;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class FirstActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+
+//        Intent intent = getIntent();
+//        String receivedText = intent.getStringExtra("word");
+//
+//        TextView textView = (TextView) findViewById(R.id.textView);
+//        textView.setText(receivedText);
+    }
+
+
+}
--- app/src/main/java/com/example/bwhsm/bramsmit_pset2/MainActivity.java
+++ app/src/main/java/com/example/bwhsm/bramsmit_pset2/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.bwhsm.bramsmit_pset2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initializeStory();
+
+    }
+
+    private void initializeStory() {
+        Story currentStory = null;
+        try {
+            InputStream is = getAssets().open("madlib0_simple.txt");
+            currentStory = new Story(is);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println(currentStory.toString());
+    }
+
+    public void goToFirst(View view) {
+
+//        EditText editText = (EditText) findViewById(R.id.editText);
+//        String text = editText.getText().toString();
+
+        Intent intent = new Intent(this, FirstActivity.class);
+//        intent.putExtra("word", text);
+
+        startActivity(intent);
+        finish();
     }
 }
